80b49f0736efce1f1d56b660af6ba873df94d019
ciamac <hgzguzg>
2020-03-23 21:25:15 +0100

    Verified by Cia v1.0.4

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+
+const app = express();
+
+//create a port to run our app on
+const PORT = process.env.PORT || 5000;
+
+//write npm run dev in terminal to start 
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
