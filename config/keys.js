1a35129e7611b2318621e74e2b71cb2fb9b19672
ciamac <hgzguzg>
2020-03-29 21:50:23 +0200

    Verified by Cia v1.1.9

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports={
-      MongoURI:'mongodb+srv://cia_login:CIALOGIN@ciamong-8eavs.mongodb.net/test?retryWrites=true&w=majority'
+      MongoURI:'mongodb+srv://ciamail:c66isaLAZYprogrammer@cluster0-y8eab.mongodb.net/test?retryWrites=true&w=majority'
+      
 } 
+//mongodb+srv:cia_login:CIALOGIN@ciamong-8eavs.mongodb.net/test?retryWrites=true&w=majority
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const bcrypt = require('bcryptjs');
+// User model
+const User = require('../models/User');
 
 //login Page
 router.get("/login", (req,res)=> res.render("login"));
@@ -34,12 +36,50 @@ router.post('/register', (req,res) =>{
               errors,
               name,
               email,
-              password,    
+              password,
+              password2    
             })
       }else {
-            res.send('pass');
+      // Validation passed
+      User.findOne({ email: email})
+      .then(user =>{
+            if(user){
+                  // User exists
+                  errors.push({ msg: 'Email is already registered'});
+                  res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2    
+            });
+      }else{
+            const newUser = new User({
+                  name,
+                  email,
+                  password
+            });
+            
+            // Hash Password
+            bcrypt.genSalt(10, (err, salt)=>
+            bcrypt.hash(newUser.password,salt,(err,hash) =>{
+                  if(err) throw err;
+                  // Set password to hashed
+                  newUser.password = hash;
+                  // Save user
+                  newUser.save()
+                  .then(user =>{
+                       res.redirect('/login'); 
+                  })
+                  .catch(err => console.log(err));  
+            })
+            )
+            console.log(newUser)
+            res.send('hello')
       }
 
+});
+      }
 });
 
 module.exports = router;
