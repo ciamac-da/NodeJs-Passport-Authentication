10629b21d82d6d92f3b6c53c356b8947d4aacbe0
ciamac <hgzguzg>
2020-04-03 18:39:31 +0200

    Verified by Ciav1.3.1

--- config/passport.js
+++ config/passport.js
@@ -15,9 +15,33 @@ module.exports = function(passport) {
                         if(!user) {
                               return done(null, false, { message: 'That email is not registered!'});
                         }
+                        // Match Password
+                        // here we need bycrypt
+                        // remember that user comes from database but password comes frpm user thats why we have to compare
+                        bycrypt.compare(password, user.password, (err, isMatch) =>{
+                              if(err) throw err;
+
+                              if(isMatch){
+                                    return done(null, user)
+                              }else{
+                                    return  done(null, false, { message: 'Password incorrect'});
+                              }
+                        });
                   })
 
                   .catch( err => console.log(err))
             })
-      )
+      );
+      // After our strategy we need a method for serializing and deserializing the USER!
+      // We have to call those methods from passportjs.org
+      passport.serializeUser((user, done)=> {
+            done(null, user.id);
+          });
+          
+          passport.deserializeUser((id, done)=> {
+            User.findById(id, (err, user)=> {
+              done(err, user);
+            });
+          }); 
+
 }
